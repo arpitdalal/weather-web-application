e9fcda058f0ac490ab28135e543cffd72e324b69
Arpit <arpit@revolution.ca>
2020-02-24 14:39:05 -0400

    insatlled hbs pckg and deleted the .html static files to use more dynamic files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather APP',
+        name: 'JSN'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About author',
+        name: 'JSN'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        message: 'call me for help!'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
