f268cc9959b1c755d8f05c5ecdd60010c196d53b
Arpit <arpit@revolution.ca>
2020-02-24 14:42:58 -0400

    changed the views folder name to 'templates' and told hbs to use 'templates' folder instead of 'views' folder

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const express = require('express')
 
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
