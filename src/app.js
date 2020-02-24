174f8ca1601a6ef2dab2a2f0afbd0c22d3c544c1
Arpit <arpit@revolution.ca>
2020-02-24 14:18:18 -0400

    added public dir with css,js,img and html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirPath = path.join(__dirname, '../public')
 
-// app.com
-// app.com/help
-// app.com/about
+app.use(express.static(publicDirPath))
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is sunny :)',
+        location: 'Charlottetown'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server initialized on port 3000 :)')
+})
