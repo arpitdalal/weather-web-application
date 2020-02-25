316252f142d11340a192277ec6abbd08d5aa4cb6
Arpit <arpit@revolution.ca>
2020-02-25 13:12:11 -0400

    imported the utils from the weather-app and got /weather working to send the correct data for the address mentioned by the user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -40,9 +42,36 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please provide an address!'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            } else {
+                res.send({
+                    forecast: forecastData,
+                    location
+                })
+            }
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search item'
+        })
+    }
     res.send({
-        forecast: 'It is sunny :)',
-        location: 'Charlottetown'
+        products: []
     })
 })
 
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/f59a17e3d1bec0ac5a10b57dec3bfbed/' + latitude + ',' + longitude + '?units=si'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Oops :O. Something went wrong!')
+        } else if (body.error) {
+            callback(body.error)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature
+                + ' degrees out. There is ' + body.currently.precipProbability
+                + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
