fa882d04e1451571aa526207a4cc28e6f29de2e3
Arpit <arpitdalal21@gmail.com>
2020-02-26 13:08:28 -0400

    added the error and success messages to be displayed directly on the browser functionality adn some styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,18 @@
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data => {
-        if (data.error) {
-            return console.log(data.error)
-        }
-        console.log(data.location, data.forecast)
-    }))
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msgDiv = document.querySelector('#msg')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    msgDiv.innerHTML = '<p>Searching...</p>'
+    const location = search.value;
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data => {
+            if (data.error) {
+                return msgDiv.innerHTML = '<p class="errorMsg">' + data.error + '</p>'
+            }
+            return msgDiv.innerHTML = '<p>In <span class="green">' + data.location + 
+                ',</span><br>It is <span class="green">' + data.forecast + '</span></p>'
+        }))
+    })
 })
